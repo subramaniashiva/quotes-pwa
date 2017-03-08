990574383b88587128a0d9081031d72c22aea6fa
subramaniashiva <vengaishiva@gmail.com>
2017-03-08 23:45:48 +0000

    Add eslint to the project

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,11 @@ module.exports = {
   },
   module: {
     rules: [{
+      enforce: "pre",
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "eslint-loader",
+    }, {
       test: /\.js$/,
       exclude: 'node_modules',
       use: ['babel-loader']
