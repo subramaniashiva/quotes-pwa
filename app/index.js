937797e89282444374261e2e64fd05b7b48ccbb3
subramaniashiva <vengaishiva@gmail.com>
2017-03-18 22:40:13 +0000

    Update index file to use container component. Updated hot reloading code

--- app/index.js
+++ app/index.js
@@ -2,45 +2,49 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-//import { AppContainer } from 'react-hot-loader';
+import { AppContainer } from 'react-hot-loader';
 
-import store from './redux/store';
+import store from 'redux/store';
 
-import Hello from './hello';
+import Main from 'containers/main';
 
 import 'styles/main.scss';
 
-/*const renderRoot = (Component) => {
+const renderRoot = (Component) => {
   render(
     <AppContainer>
-      <Component/>
+      <Provider store={store}>
+        <Component/>
+      </Provider>
     </AppContainer>,
     document.getElementById('app')
   );
 };
 
-//render(Hello);
+renderRoot(Main);
 
 // Hot Module Replacement API
-/*if (module.hot) {
-  module.hot.accept('./hello', () => {
-    const NextApp = require('./hello').default;
-    ReactDOM.render(
+if (module.hot) {
+  module.hot.accept('containers/main', () => {
+    const NextApp = require('containers/main').default;
+    render(
       <AppContainer>
+        <Provider store={store}>
          <NextApp />
+        </Provider>
       </AppContainer>,
       document.getElementById('app')
     );
   });
-}*/
+}
 
 
 
 
-render(
-  <Provider store={store}>
-    <Hello />
-  </Provider>,
-  document.getElementById('app')
-)
+// render(
+//   <Provider store={store}>
+//     <Main />
+//   </Provider>,
+//   document.getElementById('app')
+//)
 
