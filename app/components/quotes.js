67f62b161eb997b77e21a2c1bedd6748baa97908
subramaniashiva <vengaishiva@gmail.com>
2017-03-20 23:23:10 +0000

    Add classes to quote component

--- app/components/quotes.js
+++ app/components/quotes.js
@@ -1,9 +1,9 @@
 import React, { PropTypes } from 'react';
 
 const Quote = ({quote}) => (
-  <div>
-      <p>{quote.value}</p>
-      <p>{quote.author}</p>
+  <div className='quote_comp'>
+      <p className='quote_comp__text'>{quote.value}</p>
+      <p className='quote_comp__author'>- {quote.author}</p>
   </div>
 )
 
