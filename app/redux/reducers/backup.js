4fbb5f6e7de22162e148c0d1c95efb2e53005dfe
subramaniashiva <vengaishiva@gmail.com>
2017-03-29 22:43:25 +0100

    Add backup reducer

---
+++ app/redux/reducers/backup.js
@@ -0,0 +1,38 @@
+import fetch from 'isomorphic-fetch';
+
+import API from 'utils/api';
+
+export const BACKUP_RECEIVED = 'backup_received';
+const tempQuote = {
+  quote: 'A computer lets you make more mistakes faster than any other invention in human history, with the possible exceptions of handguns and tequila.',
+  author: 'Mitch Ratcliffe'
+};
+
+export function get_backup_quote() {
+  return function(dispatch) {
+    return fetch(API.root + API.path.popularQuote)
+      .then(response => response.json())
+      .then((json) => {
+        dispatch(backup_received(json));
+      })
+      .catch(() => {
+        dispatch(backup_received([tempQuote]));
+      });
+  }
+}
+
+export function backup_received(data) {
+  return {
+    type: BACKUP_RECEIVED,
+    data
+  }
+}
+
+export default function backup(state = [], action) {
+  switch(action.type) {
+    case BACKUP_RECEIVED:
+      return [...action.data];
+    default:
+      return state;
+  }
+}
