56a0380c9542f28ed5c7d8a7d12d442b8159fab3
subramaniashiva <vengaishiva@gmail.com>
2017-03-29 22:43:45 +0100

    Add backup as state tree

--- app/redux/reducers/root-reducer.js
+++ app/redux/reducers/root-reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import quotes from './quotes';
 import loading from './loading';
+import backup from './backup';
 
 export default combineReducers({
 	quotes,
-	loading
+	loading,
+  backup
 });
