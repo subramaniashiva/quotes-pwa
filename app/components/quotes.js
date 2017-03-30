ee4a8ecba3dc73b3aa12ef4b4ff60fe92b94c7f5
subramaniashiva <vengaishiva@gmail.com>
2017-03-30 22:30:37 +0100

    Strip html content in quote

--- app/components/quotes.js
+++ app/components/quotes.js
@@ -1,9 +1,10 @@
 import React, { PropTypes } from 'react';
+import * as utils from 'utils/utils';
 
 const Quote = ({quote}) => (
   <div className='quote_comp'>
-      <p className='quote_comp__text'>{quote.quote}</p>
-      <p className='quote_comp__author'>- {quote.author}</p>
+      <p className='quote_comp__text'>{utils.decodeEntities(utils.stripHtmlTags(quote.content))}</p>
+      <p className='quote_comp__author'>- {quote.title}</p>
   </div>
 )
 
